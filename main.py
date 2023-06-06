9d30668d9f813fdf2f73857e7f078d96128505bb
James Simmons <simmonsj330@gmail.com>
2023-06-06 16:21:54 -0500

    adding simple model to both mains

---
+++ main.py
@@ -0,0 +1,24 @@
+from sklearn import datasets
+from sklearn.model_selection import train_test_split
+from sklearn import svm
+from sklearn import metrics
+
+import mlflow
+
+if __name__ == '__main__':
+    
+    # load in dataset
+    diabetes = datasets.load_breast_cancer(as_frame=True)
+
+    # train, test, split
+    X_train, X_test, Y_train, Y_test = train_test_split(diabetes.data, diabetes.target, test_size=0.3, random_state=109)
+    
+    # call classifier, fit, and run prediction
+    clf = svm.SVC(C=2, kernel="linear")
+    clf.fit(X_train, Y_train)
+    Y_pred = clf.predict(X_test)
+
+    #print(clf.get_params())
+
+    # identify accuracy
+    print("Accuracy:",metrics.accuracy_score(Y_test, Y_pred))
